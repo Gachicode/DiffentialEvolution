459d7b9ee0c26d5bbd690635adf493403e4df3c6
Ko1baser <astarikov541@gmail.com>
2023-05-17 00:23:20 +0300

    ветка с оптимальными параметрами

--- DiffentialEvolution/DiffentialEvolution.cpp
+++ DiffentialEvolution/DiffentialEvolution.cpp
@@ -9,10 +9,10 @@
 #include "Functions.h"
 #include <math.h>
 
-int BUILD_PLOT      = 0;
-int FUNC_TYPE       = -1;
-int PRINT_VALUES    = 0;
-int PRINT_RESULTS   = 0;
+int BUILD_PLOT = 0;
+int FUNC_TYPE = -1;
+int PRINT_VALUES = 0;
+int PRINT_RESULTS = 0;
 
 double c[MAX_POPULATION][MAX_GENS], d[MAX_POPULATION][MAX_GENS];
 double oldarray[MAX_POPULATION][MAX_GENS];
@@ -32,12 +32,12 @@ std::vector<double> general_main(int strategy, int genmax, int D, int NP, double
     int   gen;             // generation counter
     long  nfeval;          // number of function evaluations     
     double trial_energy;    // buffer variable                    
-    double  tmp[MAX_GENS]       = { 0 }, 
-            best[MAX_GENS]      = { 0 },
-            bestit[MAX_GENS]    = { 0 }; // members  
+    double  tmp[MAX_GENS] = { 0 },
+        best[MAX_GENS] = { 0 },
+        bestit[MAX_GENS] = { 0 }; // members  
     double energy[MAX_POPULATION] = { 0 };  // obj. funct. values                 
     double emin;            // help variables                     
-    
+
     // Uniform real distribution
     std::random_device rand;
     std::mt19937 engine(rand()); // A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits.
@@ -191,7 +191,7 @@ std::vector<double> general_main(int strategy, int genmax, int D, int NP, double
                         tmp[n] = bestit[n] + (oldarray[r1][n] + oldarray[r2][n] - oldarray[r3][n] - oldarray[r4][n]) * F;
                         tmp[n] = check_bounds(tmp[n], inibound_h, inibound_l);
                     }
-                        
+
                     n = (n + 1) % D;
                 }
             }
@@ -226,7 +226,7 @@ std::vector<double> general_main(int strategy, int genmax, int D, int NP, double
         best_points_x.push_back(best[0]);
         best_points_y.push_back(best[1]);
         best_points_z.push_back(emin);
-        
+
         copy_vector(bestit, best);  // Save best population member of current iteration
 
         // swap population arrays. New generation becomes old one
@@ -246,7 +246,7 @@ std::vector<double> general_main(int strategy, int genmax, int D, int NP, double
         std::cout << std::format("Strategy: {}  NP: {}  F: {}  CR: {}\n", strategy, NP, F, CR);
         std::cout << std::format("Elapsed time: {}\n", std::chrono::duration_cast<std::chrono::milliseconds>(end - start));
     }
-    
+
     std::vector<double> total_result;
 
     for (int i = 0; i < D; i++)
@@ -254,8 +254,8 @@ std::vector<double> general_main(int strategy, int genmax, int D, int NP, double
 
     total_result.push_back(emin);
     total_result.push_back((double)strategy);
-    total_result.push_back((double)NP); 
-    total_result.push_back(F); 
+    total_result.push_back((double)NP);
+    total_result.push_back(F);
     total_result.push_back(CR);
 
     return total_result;
@@ -301,73 +301,73 @@ int main(int argc, char* argv[]) {
     double acceptable_error = 0.05; //if error equals 1% or less -> count as converged
     int repeats = 250; //repeat 
 
-    double F, CR, NP_multi;
-    int iters = 200;
-    NP_multi = 10; //We get num of parents by multiplication amount of genes by this number
-    F = 1;
-    
+    double F, CR;
+    int iters = 100;
+    F = 0.7; //We get num of parents by multiplication amount of genes by this number
+    CR = 0.12;
+    int NP = 80;
+
     std::ofstream myfile;
-    myfile.open("example_CR.csv");
-    myfile << "CR; strategy 1 convergence; strategy 1 MAPE; strategy 2 convergence; strategy 2 MAPE; strategy 3 convergence; strategy 3 MAPE\n";
+    myfile.open("optimal_parameters.csv");
+    myfile << ";convergence; MAPE\n";
     // all conv and MAPE in %
     //strat 1 - base
     //strat 2 - our mod 
     //strat 3 - our mod with 4 parents
-    for (CR = 0; CR <= 1 + 0.01; CR += 0.01)
+
+    std::string total_string = "";
+    //for(int iters = 10; iters <= 500; iters +=10)
+    //{
+    //total_string = std::to_string(NP) + ";";
+    //std::vector<std::vector<double>> summary;
+    for (size_t strategy = 1; strategy <= 3; strategy++)
     {
         std::string total_string = "";
-        //for(int iters = 10; iters <= 500; iters +=10)
-        //{
-        total_string = std::to_string(CR) + ";";
-        //std::vector<std::vector<double>> summary;
-        for (size_t strategy = 1; strategy <= 3; strategy++)
+        //summary.clear();
+        int converged = 0, counter = 0;
+        double MAPE = 0;
+        //summary.push_back(general_main(strategy, iters, D, D * NP_multi, UPPER_BOUND, LOWER_BOUND, 1, 0.5));
+
+
+        //for (double F = 0; F <= 2; F += 0.2)
+        for (int a = 0; a < repeats; a++)
         {
-            //summary.clear();
-            int converged = 0, counter = 0;
-            double MAPE = 0;
-            //summary.push_back(general_main(strategy, iters, D, D * NP_multi, UPPER_BOUND, LOWER_BOUND, 1, 0.5));
+            counter++;
+            //summary.push_back(general_main(strategy, iters, D, D * NP_multi, UPPER_BOUND, LOWER_BOUND, F, CR));
+            double func_result = general_main(strategy, iters, D, NP, UPPER_BOUND, LOWER_BOUND, F, CR)[D];
+            MAPE += fabs(egg_holder_optimum - func_result);
+            //std::cout << summary.back()[D] << '\n';
+            if (fabs(func_result) > fabs((1 - acceptable_error) * egg_holder_optimum)) // check if value in right interval (95%)
+                converged++;
 
-
-            //for (double F = 0; F <= 2; F += 0.2)
-            for (int a = 0; a < repeats; a++)
-            {
-                counter++;
-                //summary.push_back(general_main(strategy, iters, D, D * NP_multi, UPPER_BOUND, LOWER_BOUND, F, CR));
-                double func_result = general_main(strategy, iters, D, D * NP_multi, UPPER_BOUND, LOWER_BOUND, F, CR)[D];
-                MAPE += fabs(egg_holder_optimum - func_result);
-                //std::cout << summary.back()[D] << '\n';
-                if (fabs(func_result) > fabs((1 - acceptable_error) * egg_holder_optimum)) // check if value in right interval (95%)
-                    converged++;
-
-            }
-            double convergeance_rate = (double)converged / (double) counter;
-
-
-    /*        std::sort(
-                summary.begin(),
-                summary.end(),
-                [](auto& left, auto& right) { return left[D] < right[D]; }
-            );*/
-
-
-            //std::cout << std::format("\n\nStrategy: {}\n", summary[0][D + 1]);
-            //for (size_t i = 0; i < 3; i++)
-            //{
-            //    std::cout << std::format("\nBest {} obj. funct. value: {:.10f}", i + 1, summary[i][D]);
-            //    //for (int j = 0; j < D; j++)
-            //    //    std::cout << std::format("\nbest[{}]: {:.7f}", j, summary[i][j]);
-            //    //std::cout << std::format("\nNP: {}  F: {:.1f}  CR: {:.1f}\n", summary[i][D + 2], summary[i][D + 3], summary[i][D + 4]);
-
-            //}
-            MAPE = MAPE / counter / fabs(egg_holder_optimum);
-            //std::cout << std::format("\nConverged: {:2f} %", convergeance_rate * 100)
-            //    << "\nMAPE: " << MAPE * 100 << '%';
-            total_string = total_string + std::to_string(convergeance_rate * 100) + ";" + std::to_string(MAPE * 100) + ";";
-            //}
         }
-        myfile << total_string << '\n';
-        std::cout << "CR=" << CR << " iteration with 3 strats is over\n";
+        double convergeance_rate = (double)converged / (double)counter;
+        //std::sort(
+        //    summary.begin(),
+        //    summary.end(),
+        //    [](auto& left, auto& right) { return left[D] < right[D]; }
+        //);
+        //std::cout << std::format("\n\nStrategy: {}\n", summary[0][D + 1]);
+        //for (size_t i = 0; i < 3; i++)
+        //{
+        //    std::cout << std::format("\nBest {} obj. funct. value: {:.10f}", i + 1, summary[i][D]);
+        //    //for (int j = 0; j < D; j++)
+        //    //    std::cout << std::format("\nbest[{}]: {:.7f}", j, summary[i][j]);
+        //    //std::cout << std::format("\nNP: {}  F: {:.1f}  CR: {:.1f}\n", summary[i][D + 2], summary[i][D + 3], summary[i][D + 4]);
+        //}
+        MAPE = MAPE / counter / fabs(egg_holder_optimum);
+        //std::cout << std::format("\nConverged: {:2f} %", convergeance_rate * 100)
+        //    << "\nMAPE: " << MAPE * 100 << '%';
+        total_string = total_string + std::to_string(convergeance_rate * 100) + ";" + std::to_string(MAPE * 100) + ";";
+        //}
+        myfile << "strategy " << strategy << ";" << total_string << '\n';
+        std::cout << "NP=" << NP << " iteration with 3 strats is over\n";
+        std::cout << total_string << "\n";
     }
+    /*std::cout << total_string;
+    myfile << total_string;*/
+    std::cout << "NP=" << NP << " iteration with 3 strats is over\n";
+
     myfile.close();
 
     return 0;
